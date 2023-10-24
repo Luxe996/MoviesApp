ec954e60d68d432616e2a86b2fa22a7ffce0e62a
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2023-10-24 16:02:42 +0300

    second step (add loader, catch errors)

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,17 +2,19 @@ import './MovieCard.css'
 import { format } from 'date-fns'
 const MovieCard = (props) => {
   const {
-    filmTitle,
-    filmOverview,
+    title,
+    overview,
     filmReleaseDate,
-    filmPosterPath,
-    filmVoteAverage,
+    poster_path,
+    vote_average,
     // filmGenres,
     // filmChangeRating,
     // filmRating,
   } = props
 
-  const filmPoster = `https://image.tmdb.org/t/p/original${filmPosterPath}`
+  console.log(props)
+
+  const filmPoster = `https://image.tmdb.org/t/p/original${poster_path}`
 
   let dateFormatted
   try {
@@ -31,13 +33,13 @@ const MovieCard = (props) => {
 
   return (
     <li className="card">
-      <img className="card__poster" src={filmPosterPath ? filmPoster : 'noFilmPoster'} alt="Постер фильма" />
+      <img className="card__poster" src={poster_path ? filmPoster : 'noFilmPoster'} alt="Постер фильма" />
       <div className="card__info">
-        <h3 className="info__title">{filmTitle}</h3>
+        <h3 className="info__title">{title}</h3>
         <p className="info__date">{dateFormatted}</p>
         <ul className="info__genres"></ul>
-        <p className="info__description">{textAbbreviation(filmOverview)}</p>
-        <span className="info__rating"> {filmVoteAverage}</span>
+        <p className="info__description">{textAbbreviation(overview)}</p>
+        <span className="info__rating"> {vote_average}</span>
       </div>
     </li>
   )
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,54 @@
 import './App.css'
 import { Component } from 'react'
+import { Spin, Alert } from 'antd'
 
 import MoviesList from '../MoviesList/MoviesList'
+import TMDBService from '../../services/services'
 
 export default class App extends Component {
+  tmdbService = new TMDBService()
+  state = {
+    movies: [],
+    isLoading: true,
+    error: false,
+    message: '',
+  }
+
+  componentDidMount() {
+    this.updateFilms()
+  }
+  handleError = (error) => {
+    this.setState({
+      movies: [],
+      isLoading: false,
+      error: true,
+      message: error.message,
+    })
+  }
+  updateFilms = () => {
+    this.tmdbService
+      .getFilms()
+      .then(({ moviesSearch }) => {
+        this.setState({
+          movies: moviesSearch,
+          isLoading: false,
+        })
+      })
+      .catch(this.handleError)
+  }
+
   render() {
+    const { movies, isLoading, error, message } = this.state
+
+    const spinner = isLoading ? <Spin /> : null
+    const alert = error ? <Alert message={message} type="error" /> : null
+    const hasData = !(isLoading || error || movies === [])
+    const data = hasData ? <MoviesList moviesList={movies} /> : null
     return (
       <div className="wrapper">
         <header className="header"></header>
         <main className="main">
-          <MoviesList />
+          {spinner} {data} {alert}
         </main>
       </div>
     )
